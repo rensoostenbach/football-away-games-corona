b553fa3ddd558ec6f3af84b54d374b3e7f2a856c
Rens <rensoostenbach@hotmail.com>
2020-11-18 12:48:44 +0100

    Weekly data collection - 18-11-2020

--- collectdata.bat
+++ collectdata.bat
@@ -1,3 +1,5 @@
 call conda activate soccer
+set Pathname="D:\Rens\Hobby\football-away-games-corona\football-away-games-corona"
+cd /d %Pathname%
 "D:\Aanaconda3\envs\soccer\python.exe" "D:\Rens\Hobby\football-away-games-corona\football-away-games-corona\data.py"
 pause
