df73e96f3647d8abbf9484601b293af16dd841da
Rens <rensoostenbach@hotmail.com>
2020-11-18 19:38:43 +0100

    Include sidebar and sidebar styling

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import dash
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
@@ -8,15 +9,96 @@ import numpy as np
 import os
 from functions import fill_df_teams
 
-API_SOCCER = os.environ.get("API_SOCCER")
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX])
 
 server = app.server
 
-app.layout = html.Div([
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.H2("Options", className="display-4", style={'font-size': '200%'}),
+        html.Hr(),
+        html.P(
+            "Choose different options in this sidebar", className="lead"
+        ),
+        dbc.FormGroup(
+            [
+                dbc.Label('Pre/Post corona or separate seasons'),
+                dbc.RadioItems(
+                    id='prepostcorona',
+                    options=[
+                        {'label': 'Pre/Post corona', 'value': 'prepost'},
+                        {'label': 'Separate seasons', 'value': 'separate'}
+                    ],
+                    value='prepost',
+                    custom=False
+                ),
+            ]
+        ),
+        dbc.FormGroup(
+            [
+                dbc.Label('League selection'),
+                dbc.RadioItems(
+                    id='leagueselector',
+                    options=[
+                        {'label': 'Premier League', 'value': 'PL'},
+                        {'label': 'Bundesliga', 'value': 'BL1'},
+                        {'label': 'Eredivisie', 'value': 'DED'},
+                        {'label': 'Serie A', 'value': 'SA'},
+                        {'label': 'Ligue 1', 'value': 'FL1'}
+                    ],
+                    value='PL',
+                    custom=False
+
+                ),
+            ]
+        ),
+        dbc.FormGroup(
+            [
+                # TODO: Preprocess the pre and post corona datasets, and think of a way how to combine different seasons/leagues
+                dbc.Label('Year selection'),
+                dbc.RadioItems(
+                    id='yearselector',
+                    options=[
+                        {'label': '2018-2019', 'value': '2018'},
+                        {'label': '2019-2020', 'value': '2019'},
+                        {'label': '2020-2021', 'value': '2020'}
+                    ],
+                    value='2020',
+                    custom=False
+                ),
+            ]
+        ),
+        dbc.FormGroup(
+            [
+                # TODO: Preprocess the pre and post corona datasets, and think of a way how to combine different seasons/leagues
+                html.Label('Pick a team to see the amount of home/away wins and draws they have.'),
+                dcc.Dropdown(id='teamselector')
+            ]
+        )
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div([
     html.Div(children=[
         html.H1(children='Football with and without fans', style={
             'textAlign': 'center',
@@ -30,54 +112,24 @@ app.layout = html.Div([
         }
                  )]),
 
-    html.Div([
-
-        html.Label('League selection'),
-        dcc.RadioItems(
-            id='leagueselector',
-            options=[
-                {'label': 'Premier League', 'value': 'PL'},
-                {'label': 'Bundesliga', 'value': 'BL1'},
-                {'label': 'Eredivisie', 'value': 'DED'},
-                {'label': 'Serie A', 'value': 'SA'},
-                {'label': 'Ligue 1', 'value': 'FL1'}
-            ],
-            value='PL'
-        )], style={'width': '15%', 'display': 'inline-block'}),
-
-    html.Div([
-        # TODO: Preprocess the pre and post corona datasets, and think of a way how to combine different seasons/leagues
-
-        html.Label('Year selection'),
-        dcc.RadioItems(
-            id='yearselector',
-            options=[
-                {'label': '2018-2019', 'value': '2018'},
-                {'label': '2019-2020', 'value': '2019'},
-                {'label': '2020-2021', 'value': '2020'}
-            ],
-            value='2020'
-        )], style={'width': '15%', 'display': 'inline-block'}),
-
     html.Div(
         dcc.Graph(
             id='winner_graph',
         )),
-
-    html.Div([
-        # TODO: Preprocess the pre and post corona datasets, and think of a way how to combine different seasons/leagues
-
-        html.Label('Pick a team to see the amount of home/away wins and draws they have.'),
-        dcc.Dropdown(
-            id='teamselector'
-        )], style={'width': '30%', 'display': 'inline-block'}),
 
     html.Div(
         dcc.Graph(
             id='teamwinner_graph',
         )
     )
-])
+],id="page-content", style=CONTENT_STYLE)
+
+app.layout = html.Div([sidebar, content])
+
+@app.callback(
+    Output(F)
+)
+
 
 @app.callback(
     Output('teamselector', 'options'),
@@ -87,6 +139,7 @@ app.layout = html.Div([
 def set_teamselector_options(league, year):
     df = pd.read_pickle(f'data/{league}_{year}.pickle')
     return [{'label': team, 'value': team} for team in np.sort(df['homeTeamName'].unique())]
+
 
 @app.callback(
     Output('winner_graph', 'figure'),
@@ -106,7 +159,7 @@ def update_winner_graph(league, year):
     Output('teamwinner_graph', 'figure'),
     [Input('leagueselector', 'value'),
      Input('yearselector', 'value'),
-    Input('teamselector', 'value')]
+     Input('teamselector', 'value')]
 )
 def update_teamwinner_graph(league, year, teamname):
     df = pd.read_pickle(f'data/{league}_{year}.pickle')
